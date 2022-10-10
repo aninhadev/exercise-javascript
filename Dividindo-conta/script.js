49d94a50b81fa01e3915c84cb6644a57fabdf9bb
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-10-10 10:44:37 -0300

    estilizando a aplicação

--- Dividindo-conta/script.js
+++ Dividindo-conta/script.js
@@ -1,10 +1,9 @@
-
 function enviar() {
 
-var pessoas = document.getElementById("qtdPessoas").value;
-var valorConta =  document.getElementById("conta").value;
-
-var total = (Number(pessoas) / Number(valorConta))
-document.getElementById("innerHTMLtxt").innerHTML = `Olá, o valor para cada pessoa pagar é ${Number(total)}`;
-
-}
+    var pessoas = document.getElementById("qtdPessoas").value;
+    var valorConta =  document.getElementById("conta").value;
+    
+    var total = (Number(valorConta) / Number(pessoas)).toFixed(2);
+    document.getElementById("innerHTMLtxt").innerHTML = `Olá, o valor para cada pessoa pagar é ${Number(total)}`;
+    
+    }
