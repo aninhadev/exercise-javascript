b66c04b78cc836f5066be8082e75bc6f447bedc7
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-10-20 14:54:53 -0300

    exercicios operdores

---
+++ somasimples/js/script.js
@@ -0,0 +1,22 @@
+var n1 = document.getElementById("n1").value;
+var n2 = document.getElementById("n2").value;
+
+
+function calcular(){
+    switch (document.getElementById("")) {
+        case 'mais':
+          n1 + n2;
+          break;
+        case 'menos':
+          n1 - n2;
+          break;
+        case 'multiplicar':
+           n1 * n2;
+          break;
+        case 'dividir':
+         n1 / n2;
+
+      }
+
+}
+
