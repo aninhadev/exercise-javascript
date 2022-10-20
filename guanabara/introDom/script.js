80be08bffbb2e0d4af1007e6d7a686715f09d78a
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-10-20 17:54:52 -0300

    Atualizando lista de exercicios

---
+++ guanabara/introDom/script.js
@@ -0,0 +1,7 @@
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0]
+
+var d = window.document.getElementById('msg')
+
+d.style.background = 'green'
+d.innerText = "aguardando..."
