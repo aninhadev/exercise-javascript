f817b135552f74cda8fa694c036777b5953b2068
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-10-25 21:36:31 -0300

    exercicio da aula 12

---
+++ guanabara/aula12/ex014.js
@@ -0,0 +1,10 @@
+var hora = 20 ;
+
+if (hora < 12) {
+  console.log(`São ${hora}:00 horas, Bom dia!`);
+} else 
+  if (hora <= 18) {
+    console.log(`São ${hora}:00 horas, Boa tarde! `);
+  } else {
+    console.log(`São ${hora}:00 horas, Boa noite! `);
+  }
