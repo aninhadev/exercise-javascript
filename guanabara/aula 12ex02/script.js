412c37fd2184ad3023243d0e1dd3ef701553d347
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-11-02 23:48:26 -0300

    exercício hora do dia

--- guanabara/aula 12ex02/script.js
+++ guanabara/aula 12ex02/script.js
@@ -1,4 +1,4 @@
-var agor = new Date();
+var agor =  new Date();
 var hora = agor.getHours();
 let body = document.querySelector('body');
 let section = document.querySelector('section');
@@ -8,11 +8,26 @@ let h1 = document.querySelector('h1')
 switch (hora) {
   case hora < 12:
     agora.innerHTML = `São ${hora}:00 horas, Bom dia!`;
+    body.style.backgroundColor = '#0353A4';
+    section.style.backgroundColor = '#FABC2A'
+    h1.style.color = 'black'
+    agora.style.cssText = 
+    'color: black;' +
+    'font-size: 25px;';
+    window.document.getElementById("img").src = "./img/nascer.jpg";
     break;
+
   case hora <= 18:
     agora.innerHTML = `São ${hora}:00 horas, Boa tarde! `;
-    $('body').css('background', '#ccc');
+    body.style.backgroundColor = '#DD7230';
+    section.style.backgroundColor = '#F5E663'
+    h1.style.color = '#FFF'
+    agora.style.cssText = 
+    'color: white;' +
+    'font-size: 25px;';
+    window.document.getElementById("img").src = "./img/entardecer.jpg";
     break;
+
   default:
     agora.innerHTML = `São ${hora}:00 horas, Boa noite! `;
     body.style.backgroundColor = '#0D1321';
@@ -21,9 +36,7 @@ switch (hora) {
     agora.style.cssText = 
     'color: white;' +
     'font-size: 25px;';
-
-
-
+    window.document.getElementById("img").src = "./img/700-18.jpg";
     break;
 }
 
