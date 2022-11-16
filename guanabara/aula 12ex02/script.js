0fdb35d33561d91786e9e5c403f6bbdcaa72e37f
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-11-16 09:25:16 -0300

    ;

--- guanabara/aula 12ex02/script.js
+++ guanabara/aula 12ex02/script.js
@@ -1,11 +1,11 @@
 var agor = new Date();
-var hora =  agor.getHours();
+var hora = agor.getHours();
 let body = document.querySelector("body");
 let section = document.querySelector("section");
 let h1 = document.querySelector("h1");
 
-/*
-switch (hora) {
+
+switch (true) {
   case hora < 12:
     agora.innerHTML = `São ${hora}:00 horas, Bom dia!`;
     body.style.backgroundColor = '#0353A4';
@@ -29,6 +29,7 @@ switch (hora) {
     break;
 
   default:
+    debugger
     agora.innerHTML = `São ${hora}:00 horas, Boa noite! `;
     body.style.backgroundColor = '#0D1321';
     section.style.backgroundColor = '#3E5C76'
@@ -40,30 +41,30 @@ switch (hora) {
     break;
 }
 
-*/
 
-if (hora < 12) {
-  agora.innerHTML = `São ${hora}:00 horas, Bom dia!`;
 
-  body.style.backgroundColor = "#0353A4";
-  section.style.backgroundColor = "#FABC2A";
-  h1.style.color = "black";
-  agora.style.cssText = "color: black;" + "font-size: 25px;";
-  window.document.getElementById("img").src = "./img/nascer.jpg";
-} else if (hora <= 18) {
-  agora.innerHTML = `São ${hora}:00 horas, Boa tarde! `;
+// if (hora < 12) {
+//   agora.innerHTML = `São ${hora}:00 horas, Bom dia!`;
 
-  body.style.backgroundColor = "#DD7230";
-  section.style.backgroundColor = "#F5E663";
-  h1.style.color = "#FFF";
-  agora.style.cssText = "color: white;" + "font-size: 25px;";
-  window.document.getElementById("img").src = "./img/entardecer.jpg";
-} else {
-  agora.innerHTML = `São ${hora}:00 horas, Boa noite! `;
+//   body.style.backgroundColor = "#0353A4";
+//   section.style.backgroundColor = "#FABC2A";
+//   h1.style.color = "black";
+//   agora.style.cssText = "color: black;" + "font-size: 25px;";
+//   window.document.getElementById("img").src = "./img/nascer.jpg";
+// } else if (hora <= 18) {
+//   agora.innerHTML = `São ${hora}:00 horas, Boa tarde! `;
 
-  body.style.backgroundColor = "#0D1321";
-  section.style.backgroundColor = "#3E5C76";
-  h1.style.color = "#FFF";
-  agora.style.cssText = "color: white;" + "font-size: 25px;";
-  window.document.getElementById("img").src = "./img/700-18.jpg";
-}
+//   body.style.backgroundColor = "#DD7230";
+//   section.style.backgroundColor = "#F5E663";
+//   h1.style.color = "#FFF";
+//   agora.style.cssText = "color: white;" + "font-size: 25px;";
+//   window.document.getElementById("img").src = "./img/entardecer.jpg";
+// } else {
+//   agora.innerHTML = `São ${hora}:00 horas, Boa noite! `;
+
+//   body.style.backgroundColor = "#0D1321";
+//   section.style.backgroundColor = "#3E5C76";
+//   h1.style.color = "#FFF";
+//   agora.style.cssText = "color: white;" + "font-size: 25px;";
+//   window.document.getElementById("img").src = "./img/700-18.jpg";
+// }
