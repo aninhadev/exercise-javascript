38a67f0e599b8b6cb267d268d439a787c31dcda0
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-12-09 06:30:07 -0300

    Exercise

--- guanabara/aula 15/script.js
+++ guanabara/aula 15/script.js
@@ -1,10 +1,23 @@
-let num = [1, 6 , 4]
+let num = [1, 6 ,2,5, 4]
+
 // num[3] = 19  inserir valor no indice desejado
 // num.push(8)  inserir valor no ultimo elemento do array
 // num.length   Para saber a quantidade de elementos de um array
 // num.sort()   Coloca os elementos em ordem crescente 
-num.push(8)
-num.sort()
-console.log(`Nosso arrayzinho é ${num}`)
-console.log(`O nosso array tem  ${num.length} posições `)
-console.log(` O primeiro valor do vetor é ${num[0]}`)
+// num.push(8)
+// num.sort()
+// console.log(`Nosso arrayzinho é ${num}`)
+// console.log(`O nosso array tem  ${num.length} posições `)
+// console.log(` O primeiro valor do vetor é ${num[0]}`)
+
+
+// for(let pos = 0; pos < num.length; pos++){
+//     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+// }
+
+// num.indexOf()    Vai procurar onde está o valor e retorna a chave 
+
+for( let pos in num ){
+    console.log(`${num[pos]}`)
+}
+
