f4f87fc878173a351aff65ad3a39685ace0e023b
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-12-09 06:17:11 -0300

    Exercise

---
+++ guanabara/aula 15/script.js
@@ -0,0 +1,10 @@
+let num = [1, 6 , 4]
+// num[3] = 19  inserir valor no indice desejado
+// num.push(8)  inserir valor no ultimo elemento do array
+// num.length   Para saber a quantidade de elementos de um array
+// num.sort()   Coloca os elementos em ordem crescente 
+num.push(8)
+num.sort()
+console.log(`Nosso arrayzinho é ${num}`)
+console.log(`O nosso array tem  ${num.length} posições `)
+console.log(` O primeiro valor do vetor é ${num[0]}`)
