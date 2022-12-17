acef46523e2fb83b81922497c74e0a80bf8695f1
Ana Maria Neves <anamarianevesdev@gmail.com>
2022-12-16 21:30:09 -0300

    Exercise

--- guanabara/analisador/ambiente.js
+++ guanabara/analisador/ambiente.js
@@ -1,27 +1,65 @@
 
 
-let numero = document.getElementById('nume')
 
-if(numero <= 0 || numero > 100 ){
- console.log('Erro! Numero invalido')
-}else{
- 
-let sum = 0;
+function adicionar() {
+    let n1 = document.getElementById("nume").value;
+  // let tab = document.getElementById('tabela').value;
 
-for( let i = 0 ; i < numero.length ; i++){
-    sum += numero [i];
+  if (numero > 100 || numero == "" || numero <= 0) {
+    alert(" ERRO!  Valor invalido! ");
+  } else {
+    const node = document.createElement("option");
+    const textnode = document.createTextNode(`Adicionado o numero ${numero}`);
+    node.appendChild(textnode);
+    document.getElementById("tabela").appendChild(node);
+  }
 }
 
+function analisar() {
+    let numero = document.getElementById("nume").value;
+  
+  let sum = 0;
 
-let media = Number(sum) / Number(numero.length);
+  for (let i = 0; i < numero.length; i++) {
+    sum += numero[i];
 
-let min = Math.min(...numero);
-let max = Math.max(...numero);
+    let media = Number(sum) / Number(numero.length);
 
-console.log(`Meu primeiro array ${numero}`)
-console.log(`Ao todo, temos ${numero.length} números cadastrados!`)
-console.log(`O maior número é ${max} `)
-console.log(`O menor numero é ${min}`)
-console.log(` a soma desses numeros é ${sum}`)
-console.log(`a media dos numero é ${media}`)
+    let min = Math.min(...numero);
+    let max = Math.max(...numero);
+
+  }
+  res.innerHTML = ` <p> Ao todo, temos ${numero.length} números cadastrados! </p>`;
 }
+
+////////////////////////////
+
+// while (numero >= 100) {
+//     let item = document.createElement("option");
+//     item.value = `tab${numero}`;
+//     tab.appendChild(item);
+//   }
+
+///////////////////////////////
+
+// if (numero <= 0 || numero > 100) {
+//   console.log("Erro! Numero invalido");
+// } else {
+//   let sum = 0;
+
+//   for (let i = 0; i < numero.length; i++) {
+//     sum += numero[i];
+//   }
+
+//   let media = Number(sum) / Number(numero.length);
+
+//   let min = Math.min(...numero);
+//   let max = Math.max(...numero);
+
+//   console.log(`Meu primeiro array ${numero}`);
+//   console.log(`Ao todo, temos ${numero.length} números cadastrados!`);
+//   console.log(`O maior número é ${max} `);
+//   console.log(`O menor numero é ${min}`);
+//   console.log(` a soma desses numeros é ${sum}`);
+//   console.log(`a media dos numero é ${media}`);
+// }
